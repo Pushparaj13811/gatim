d9d50a54cca7377cd72e0a99b4a0e6995aad98f5
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-12-20 22:07:55 +0530

    Add translation service with document and content translation functions

--- src/services/translationService.ts
+++ src/services/translationService.ts
@@ -1,5 +1,7 @@
 import { api } from '@/lib/api';
 import { ApiError } from '@/lib/errors';
+import { markdownToHtml } from '@/utils/htmlConverter';
+import { htmlToMarkdown } from '@/utils/markdownConverter';
 
 export interface TranslationParams {
     from_lang: string;
@@ -15,15 +17,20 @@ export interface TranslationResult {
 }
 
 export const translateDocument = async (params: TranslationParams): Promise<TranslationResult> => {
+
+    const markdownContent = htmlToMarkdown(params.content)
+
     try {
         const response = await api.translateDocument({
             from_lang: params.from_lang,
             to_lang: params.to_lang,
-            content: params.content,
+            content: markdownContent,
         });
 
+        const resultContent = await markdownToHtml(response.data.translated_content)
+
         return {
-            translatedContent: response.data.translated_content,
+            translatedContent: resultContent,
             from_lang: response.data.from_language,
             to_lang: response.data.to_language,
         };
